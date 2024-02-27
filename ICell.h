493c4ef58f7ba050aa7fd09685a1a2d6edcc4b80
simoneBB12 <60448725+simoneBB12@users.noreply.github.com>
2024-02-27 13:18:47 -0700

    Create ICell.h

---
+++ ICell.h
@@ -0,0 +1,10 @@
+#ifndef ICELLH
+#define ICELLH
+
+#include "Cell.h"
+
+//industrial function declaration
+void IndustrialUpdate(int numRows, int numCols, int& availableWorkers, int& availableGoods, Cell* regionMap[][100], bool &radiationInhibitUpdate);
+
+
+#endif
