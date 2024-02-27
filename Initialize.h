c6f2120b8ece9cf77f9f5258ac4a133dbba675d2
simoneBB12 <60448725+simoneBB12@users.noreply.github.com>
2024-02-27 13:20:42 -0700

    Update Initialize.h

--- Initialize.h
+++ Initialize.h
@@ -1,1 +1,15 @@
+#ifndef INITIALIZE_H
+#define INITIALIZE_H
+
+#include "Cell.h"
+
+void ReadFiles(int& timeLimit, int& refreshRate, int& numCols, int& numRows, string& regionFileName, int& availWorkers, int& availGoods);
+
+void FillArray(Cell* regionMap[][100], const int numRows, const int numCols, const string regionFileName, int& availWorkers, int& availGoods);
+
+void PopulateAdjacencyLists(Cell* regionMap[][100], const int numRows, const int numCols);
+
+void DeleteArray(Cell* regionMap[][100], const int numRows, const int numCols);
+
+#endif
 
